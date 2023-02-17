0d94fe203db7287a498c7366a446e95820e16839
Basant B. Bhandari <basant.bhandari@tekvortex.local>
2023-02-17 14:07:59 +0545

    initail commit

---
+++ auth1/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using auth1.Data;
+using auth1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace auth1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private ApplicationDbContext _context;
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext applicationDbContext)
+        {
+            _logger = logger;
+            _context = applicationDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Users.ToList());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
